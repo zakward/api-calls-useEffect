e86e5378aa2632e4d638aea5cb6330a3eac1cd26
Jordan Burger <jordanburger22@gmail.com>
2024-07-29 20:17:09 +0000

    dependancy array

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,22 @@ import axios from "axios"
 
 
 function App() {
-  
+
   const [leagueChamps, setLeagueChamps] = useState([])
-  const [count, setCount] = useState(0)
-  function incCount(){
+  const [count, setCount] = useState(1)
+  function incCount() {
     setCount(prevCount => prevCount + 1)
   }
 
-// https://swapi.dev/api/people/1
-// https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json
+  // https://swapi.dev/api/people/1
+  // https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json
 
   const [starWarsData, setStarWarsData] = useState([])
 
   useEffect(() => {
-    axios.get("https://swapi.dev/api/people/")
-    .then(res => setStarWarsData(res.data.results))
-    .catch(err => console.log(err))
-    console.log('Component Mounted')
+    axios.get(`https://swapi.dev/api/people/${count}`)
+      .then(res => setStarWarsData(res.data))
+      .catch(err => console.log(err))
   }, [])
 
   console.log(starWarsData)
@@ -28,18 +27,16 @@ function App() {
   //   .then(res => console.log(Object.values(res.data.data)))
   //   .catch(err => console.log(err))
 
-
-    console.log("Component Rendered")
-
   return (
-    <div className = "container">
+    <div className="container">
+      <h1>{starWarsData.name}</h1>
       <h1>API CALLS</h1>
-        <p>axios</p>
-        <p>fetch</p>
-        <p>the response</p>
-        <p>infinite loops</p>
-        <p>useEffect</p>
-        <button onClick={incCount}>Click Me</button>
+      <p>axios</p>
+      <p>fetch</p>
+      <p>the response</p>
+      <p>infinite loops</p>
+      <p>useEffect</p>
+      <button onClick={incCount}>Click Me</button>
     </div>
   )
 }
