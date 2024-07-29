71033bfcad214e1c74f4a4cb468950f8284211da
Jordan Burger <jordanburger22@gmail.com>
2024-07-29 19:17:13 +0000

    intro'd use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from "axios"
 
 
 function App() {
   
   const [leagueChamps, setLeagueChamps] = useState([])
+  const [count, setCount] = useState(0)
+  function incCount(){
+    setCount(prevCount => prevCount + 1)
+  }
+
 // https://swapi.dev/api/people/1
 // https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json
 
-  // axios.get("https://swapi.dev/api/people/")
-  //   .then(res => console.log(res.data))
+  const [starWarsData, setStarWarsData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+    .then(res => setStarWarsData(res.data.results))
+    .catch(err => console.log(err))
+    console.log('Component Mounted')
+  }, [])
+
+  console.log(starWarsData)
+
+  // axios.get("https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json")
+  //   .then(res => console.log(Object.values(res.data.data)))
   //   .catch(err => console.log(err))
 
-  axios.get("https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json")
-    .then(res => console.log(Object.values(res.data.data)))
-    .catch(err => console.log(err))
 
     console.log("Component Rendered")
 
@@ -26,6 +39,7 @@ function App() {
         <p>the response</p>
         <p>infinite loops</p>
         <p>useEffect</p>
+        <button onClick={incCount}>Click Me</button>
     </div>
   )
 }
