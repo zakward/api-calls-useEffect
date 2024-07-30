c1d7346167714be966bf22a5ef91246f6e9ee581
Zak Ward <zakward85@gmail.com>
2024-07-30 14:22:02 -0500

    league map render

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,41 @@ import axios from "axios"
 
 function App() {
 
+  //creating state
   const [leagueChamps, setLeagueChamps] = useState([])
   const [count, setCount] = useState(1)
+  const [starWarsData, setStarWarsData] = useState([])
+  
+  //incrementing count
   function incCount() {
     setCount(prevCount => prevCount + 1)
   }
 
-  // https://swapi.dev/api/people/1
-  // https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json
+  //get request on mount
+  // useEffect(() => {
+  //   axios.get(`https://swapi.dev/api/people/${count}`)
+  //     .then(res => setStarWarsData(res.data))
+  //     .catch(err => console.log(err))
+  // }, [])
 
-  const [starWarsData, setStarWarsData] = useState([])
-
-  useEffect(() => {
-    axios.get(`https://swapi.dev/api/people/${count}`)
-      .then(res => setStarWarsData(res.data))
+  useEffect(()=> {
+    axios.get("https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json")
+      .then(res => setLeagueChamps(Object.values(res.data.data)))
       .catch(err => console.log(err))
   }, [])
 
-  console.log(starWarsData)
+  const champsList = leagueChamps.map(champ => {
+    return (
+      <>
+        <h2>{champ.name}</h2>
+        <p>{champ.blurb}</p>
+        <img src = {`https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/${champ.image.full}`}/>
+      </>
+    )
+  })
 
-  // axios.get("https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json")
-  //   .then(res => console.log(Object.values(res.data.data)))
-  //   .catch(err => console.log(err))
-
+  console.log(leagueChamps)
+  
   return (
     <div className="container">
       <h1>{starWarsData.name}</h1>
@@ -37,6 +49,7 @@ function App() {
       <p>infinite loops</p>
       <p>useEffect</p>
       <button onClick={incCount}>Click Me</button>
+      {champsList}
     </div>
   )
 }
